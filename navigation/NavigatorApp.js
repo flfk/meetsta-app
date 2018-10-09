3d7082a8e9695e3190730f821101e185e51fa37e
flfk <flfk@users.noreply.github.com>
2018-10-09 17:56:49 -0300

    Minor refactoring, base navigation implemented

--- navigation/NavigatorApp.js
+++ navigation/NavigatorApp.js
@@ -1,19 +1,4 @@
-import {
-  createSwitchNavigator,
-  createStackNavigator,
-  createBottomTabNavigator
-} from 'react-navigation';
-
-import AddTicket from '../screens/AddTicket';
-import EventsOrganiser from '../screens/EventsOrganiser';
-import CallFan from '../screens/CallFan';
-import CallOrganiser from '../screens/CallOrganiser';
-import CallEndedFan from '../screens/CallEndedFan';
-import EventEndedOrganiser from '../screens/EventEndedOrganiser';
-import Login from '../screens/Login';
-import QueueFan from '../screens/QueueFan';
-import QueueOrganiser from '../screens/QueueOrganiser';
-import Tickets from '../screens/Tickets';
+import { createSwitchNavigator } from 'react-navigation';
 
 import NavigatorAuth from './NavigatorAuth';
 import NavigatorCallFan from './NavigatorCallFan';
